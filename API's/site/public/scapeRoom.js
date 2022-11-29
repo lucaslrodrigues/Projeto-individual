49f94309a3b52295499ef9e1c91755a2d75da397
Lucas <lucas.lrodrigues@sptech.school>
2022-11-29 01:04:51 -0300

    API

--- API's/site/public/scapeRoom.js
+++
@@ -1,5 +0,0 @@
-// function game_area1_tabela(){
-//     alert('teste')
-//     game_area1.style.display = "none"
-//     game_area2.style.display = "block"
-// }
--- API's/site/src/models/avisoModel.js
+++ API's/site/src/models/avisoModel.js
@@ -3,18 +3,8 @@ var database = require("../database/config");
 function listar() {
     console.log("ACESSEI O AVISO  MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listar()");
     var instrucao = `
-        SELECT 
-            a.id AS idAviso,
-            a.titulo,
-            a.descricao,
-            a.fk_usuario,
-            u.id AS idUsuario,
-            u.nome,
-            u.email,
-            u.senha
-        FROM aviso a
-            INNER JOIN usuario u
-                ON a.fk_usuario = u.id;
+        select usuario.nome 'Nome', tentativa.tempo 'Tempo'
+	    from usuario join tentativa on idUsuario = fkUsuario order by tentativa.tempoSegundos;
     `;
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
